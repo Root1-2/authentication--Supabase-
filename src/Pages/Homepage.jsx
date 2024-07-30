5dedc79d29a5495c75ebdce5cf3afb7f00b6c43f
John Doe <abdullahalmasrur8@gmail.com>
2024-07-31 00:33:31 +0600

    design fix

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+  height: 100vh;
+  width: 100vw;
+`;
+
 function Homepage() {
-  return <div>Boom</div>;
+  return <Main>Boom</Main>;
 }
 
 export default Homepage;
